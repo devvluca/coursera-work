cd896330369a5914ed1269fae11448a2fd674c5a
Luca Aguiar <133829719+devvluca@users.noreply.github.com>
2024-03-20 18:20:59 -0300

    Update SpeakHello.js

--- coursera work/SpeakHello.js
+++ coursera work/SpeakHello.js
@@ -1,1 +1,13 @@
+(function () {
 
+    var helloSpeaker = {};
+
+    var speakWordHello = "Hello";
+
+    helloSpeaker.speak = function (name) {
+      console.log(speakWordHello + " " + name);
+    };
+
+    window.helloSpeaker = helloSpeaker;
+
+})();
